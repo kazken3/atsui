c237fc87d19b8c053e309c7b11596c1b58d00814
Kentaro KAZUHAMA <kazken3@gmail.com>
2012-05-29 07:29:22 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask, request, render_template
+from mongodb import MongoDB
 
 app = Flask(__name__)
 
@@ -9,10 +10,11 @@ app = Flask(__name__)
 
 def index():
     message = None
-    db = MongoDB(app)
+    db = MongoDB()
     message = db.get_data()
     return render_template('index.html', message=message)
 
 if  __name__ == '__main__':
+#    app.debug= True
     app.run()
 
